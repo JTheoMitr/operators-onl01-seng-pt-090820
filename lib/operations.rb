f1530f5853ff0b14efc4f9739e32a4959ea707c3
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-11 04:59:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,17 +1,11 @@
 def unsafe?(speed)
-  if speed < 40
-    return true
-  elsif speed > 60
-    return true
-  else
-    return false
-  end
+  
 end
 
 
 
 def not_safe?(speed)
-	speed < 40 || speed > 60 ? return true : return false
+
 end
 	
 
